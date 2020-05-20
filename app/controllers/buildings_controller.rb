d5fbddcef31f2b773850c9a51bb141fbc2a8a347
Paula Sturmer <paulasturmer@gmail.com>
2020-05-20 10:50:17 -0300

    Model and CRUD methods Buildings

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,46 @@
+class BuildingsController < ApplicationController
+  before_action :find_building, only: [:show, :destroy, :edit]
+
+  def index
+    @buildings = Building.all
+    # @buildings = policy_scope(Building).all -- Quando tiver pundit
+  end
+
+  def show
+  end
+
+  def new
+    @building = Building.new
+    #authorize(@building) -- Quando tiver pundit
+  end
+
+  def edit
+  end
+
+  def create
+    @building = Building.new(building_params)
+    #authorize(@building) -- Quando tiver pundit
+    @building.user = current_user
+    if @building.save
+      redirect_to buildings_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @building.destroy
+    redirect_to buildings_path
+  end
+
+  private
+
+  def find_building
+    @building = Building.find(params[:id])
+    #authorize(@building) -- Quando tiver pundit
+  end
+
+  def building_params
+    params.require(:building).permit(:building_name, :super_name, :super_email, :zipcode, :city, :province, :country)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_002051) do
+ActiveRecord::Schema.define(version: 2020_05_20_125909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buildings", force: :cascade do |t|
+    t.string "building_name"
+    t.string "super_name"
+    t.string "super_email"
+    t.integer "zipcode"
+    t.string "city"
+    t.string "province"
+    t.string "country"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_buildings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +37,11 @@ ActiveRecord::Schema.define(version: 2020_05_20_002051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "buildings", "users"
 end
