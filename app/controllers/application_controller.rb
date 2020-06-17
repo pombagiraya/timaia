c28dd92a9fe629233d76a46a637b75a9002a5966
Paula Sturmer <paulasturmer@gmail.com>
2020-06-16 21:05:51 -0300

    Conclusão avatar e correção edit schedule

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
-    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :photo])
   end
 
   def user_role
--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,13 +1,15 @@
 class AvailabilityValidator < ActiveModel::EachValidator
 
     def validate_each(record, attribute, value)
-      #schedules = Schedule.where(["room_id =? and id <> ?", record.room_id, record.id])
-      schedules = Schedule.where(["room_id = ?", record.room_id])
-      date_ranges = schedules.map { |b| b.start_time..b.end_time }
-  
-      date_ranges.each do |range|
-        if range.include? value
-          record.errors.add(attribute, "not available")
+
+      schedule = Schedule.find_by(["id = ?", record.id])
+      unless schedule.start_time == record.start_time && schedule.end_time == record.end_time
+        schedules = Schedule.where(["room_id = ?", record.room_id])
+        date_ranges = schedules.map { |b| b.start_time..b.end_time }
+        date_ranges.each do |range|
+          if range.include? value
+            record.errors.add(attribute, "not available")
+          end
         end
       end
     end
