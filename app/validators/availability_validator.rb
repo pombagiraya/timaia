15a80a530af5f02dd1f24df691f8d1cf601085ae
Paula Sturmer <paulasturmer@gmail.com>
2020-06-18 19:20:58 -0300

    Ajuste fix new schedule

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -3,7 +3,15 @@ class AvailabilityValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
 
       schedule = Schedule.find_by(["id = ?", record.id])
-      unless schedule.start_time == record.start_time && schedule.end_time == record.end_time
+      if schedule.nil?
+        schedules = Schedule.where(["room_id = ?", record.room_id])
+        date_ranges = schedules.map { |b| b.start_time..b.end_time }
+        date_ranges.each do |range|
+          if range.include? value
+            record.errors.add(attribute, "not available")
+          end
+        end
+      elsif schedule.start_time != record.start_time && schedule.end_time != record.end_time
         schedules = Schedule.where(["room_id = ?", record.room_id])
         date_ranges = schedules.map { |b| b.start_time..b.end_time }
         date_ranges.each do |range|
@@ -13,4 +21,4 @@ class AvailabilityValidator < ActiveModel::EachValidator
         end
       end
     end
-  end
+end
